c747644584b4f9ed877a789608b5f10428f36ff2
ParkKyunglim <pkl1004125@gmail.com>
2025-06-26 23:38:27 +0900

    [Day01] 사칙연산

---
+++ src/beginner/day01/몫구하기.java
@@ -0,0 +1,9 @@
+package beginner.day01;
+
+public class 몫구하기 {
+    // num1을 num2로 나눈 몫을 return
+    public int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
